569c837ad83933c028facc33d380f2cadf6f549f
setcain <charlie.bronson00@gmail.com>
2019-12-03 23:33:23 -0600

    adds GET and POST methods from APIView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from .serializers import HelloSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """
+    Test APIView
+    """
+    serializer_class = HelloSerializer
+
+    def get(self, request, format=None):
+        """
+        Return a list of APIView freatures
+        """
+        message = 'hello world'
+        an_apiview = [
+            'Use HTTP methods',
+            'Similar to traditional Django views',
+            'Give you the most control over your application logic',
+            'Mapped manually to your URLs config'
+        ]
+
+        # Response return a list or dict
+        return Response(
+            {
+                'message': message,
+                'an_apiview': an_apiview
+            }
+        )
+
+    def post(self, request):
+        """
+        Create a hello message whit serializers name
+        """
+        serializer_data = self.serializer_class(data=request.data)
+
+        if serializer_data.is_valid():
+            name = serializer_data.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer_data.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
